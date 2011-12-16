5415a917e81b524ddfdace3d62da78d6dbeb0c71
Alok Lal <alok.n.lal@gmail.com>
2011-12-16 12:16:44 -0500

    Fixed review model so context can be mass assigned and state is removed from validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,10 @@
 class Review < ActiveRecord::Base
-  validates :title, :state, :url, :context, :reviewer_id, :presence => true
+  validates :title, :url, :context, :reviewer_id, :presence => true
   belongs_to :user
   before_create :setState
   has_many :questions, :dependent => :destroy
   accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:question].blank? }, :allow_destroy => true
-  attr_accessible :questions_attributes, :reviewer_id, :url, :title, :overall_comments, :suggested_drills, :state
+  attr_accessible :questions_attributes, :reviewer_id, :url, :title, :overall_comments, :suggested_drills, :state, :context
 
 
   private
